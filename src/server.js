4df013d3ae1bf0b4bc35fe66de4419931c175621
jean <alysoonfelipe@hotmail.com>
2025-12-31 00:36:08 -0300

    mudancas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.routes.js";
 import transacoesRoutes from "./routes/transacoes.routes.js";
@@ -14,6 +16,14 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/transacoes", transacoesRoutes);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
