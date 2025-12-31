dba45653e4f41be62f410cc5417b7834c57b743d
jean <alysoonfelipe@hotmail.com>
2025-12-31 01:08:13 -0300

    fazendo conexao com front

--- src/db.js
+++ src/db.js
@@ -1,12 +1,28 @@
 import pkg from "pg";
 const { Pool } = pkg;
+import dotenv from "dotenv";
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "fluxo_caixa_tio",
-  password: "080216",
-  port: 5432,
-});
+dotenv.config();
+
+// Se houver uma DATABASE_URL (no Render), usamos ela. 
+// Caso contrário, usamos as configurações locais.
+const isProduction = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false, // Obrigatório para bancos de dados gerenciados como o do Render
+        },
+      }
+    : {
+        user: "postgres",
+        host: "localhost",
+        database: "fluxo_caixa_tio",
+        password: "080216",
+        port: 5432,
+      }
+);
 
 export default pool;
