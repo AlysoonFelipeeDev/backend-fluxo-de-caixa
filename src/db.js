07e42a2183953c4e03169f5403704994a430117e
jean <alysoonfelipe@hotmail.com>
2025-12-31 01:42:57 -0300

    banco de dados

--- src/db.js
+++ src/db.js
@@ -1,28 +1,11 @@
 import pkg from "pg";
 const { Pool } = pkg;
-import dotenv from "dotenv";
 
-dotenv.config();
-
-// Se houver uma DATABASE_URL (no Render), usamos ela. 
-// Caso contrário, usamos as configurações locais.
-const isProduction = process.env.DATABASE_URL;
-
-const pool = new Pool(
-  isProduction
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false, // Obrigatório para bancos de dados gerenciados como o do Render
-        },
-      }
-    : {
-        user: "postgres",
-        host: "localhost",
-        database: "fluxo_caixa_tio",
-        password: "080216",
-        port: 5432,
-      }
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // <--- Isso é o que permite o servidor falar com o banco
+  }
+});
 
 export default pool;
