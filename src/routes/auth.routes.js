7701920105e550952485bdea9db3385202d95c97
jean <alysoonfelipe@hotmail.com>
2025-12-31 01:41:49 -0300

    quase la

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import pool from "../db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const router = Router();
